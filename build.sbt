a7091e4510193d8a2cea415e1c7febb89e7250db
Jagath <jagathmax2@gmail.com>
2019-04-01 08:03:49 +0530

    sample queue

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ scalaVersion := "2.12.8"
 libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.4.1"
 libraryDependencies += "com.googlecode.json-simple" % "json-simple" % "1.1.1"
 libraryDependencies += "com.google.code.gson" % "gson" % "2.2.2"
+// https://mvnrepository.com/artifact/com.rabbitmq/amqp-client
+libraryDependencies += "com.rabbitmq" % "amqp-client" % "5.0.0"
--- src/main/scala/ListMenu.scala
+++ src/main/scala/ListMenu.scala
@@ -1,12 +1,35 @@
 import java.util
+
 import com.google.gson.{Gson, JsonArray, JsonParser}
+import com.rabbitmq.client.ConnectionFactory
 import scalaj.http.Http
 
 class ListMenu {
+  /*private var connection: com.rabbitmq.client.Connection = null
+  private var channel: com.rabbitmq.client.Channel  = null
+  private val requestQueueName = "rpc_queue"*/
+
+  private val QUEUE_NAME = "hello"
+
   def show() = {
     println()
     println("Book List")
     println()
+
+
+    val factory = new ConnectionFactory()
+    factory.setHost("localhost")
+    val connection = factory.newConnection()
+    val channel = connection.createChannel()
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+    val message = "Hello World!"
+    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"))
+    println(" [x] Sent '" + message + "'")
+    channel.close()
+    connection.close()
+
+
+
     val result = Http("http://localhost:8000/getAllBooks").asString
     val jsonPsr: JsonParser =new JsonParser()
     val jsonAry: JsonArray = jsonPsr.parse(result.body).asInstanceOf[JsonArray]
@@ -18,4 +41,6 @@ class ListMenu {
     println("Press Enter to go back")
     scala.io.StdIn.readLine()
   }
+
+
 }
