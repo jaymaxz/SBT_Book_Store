64b4efc5a82dfccb23ad6c54f8e27764528c898c
Jagath <jagathmax2@gmail.com>
2019-04-01 09:43:47 +0530

    list option

--- src/main/scala/ListMenu.scala
+++ src/main/scala/ListMenu.scala
@@ -1,38 +1,39 @@
 import java.util
 
 import com.google.gson.{Gson, JsonArray, JsonParser}
-import com.rabbitmq.client.ConnectionFactory
-import scalaj.http.Http
+import com.rabbitmq.client.{ConnectionFactory, DeliverCallback}
 
 class ListMenu {
-  /*private var connection: com.rabbitmq.client.Connection = null
-  private var channel: com.rabbitmq.client.Channel  = null
-  private val requestQueueName = "rpc_queue"*/
-
-  private val QUEUE_NAME = "hello"
+  private val LIST_REQ_QUEUE_NAME = "List Request Queue"
+  private val LIST_RES_QUEUE_NAME = "List Response Queue"
 
   def show() = {
     println()
     println("Book List")
     println()
 
-
     val factory = new ConnectionFactory()
     factory.setHost("localhost")
     val connection = factory.newConnection()
     val channel = connection.createChannel()
-    channel.queueDeclare(QUEUE_NAME, false, false, false, null)
-    val message = "Hello World!"
-    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"))
-    println(" [x] Sent '" + message + "'")
-    channel.close()
-    connection.close()
+    channel.queueDeclare(LIST_REQ_QUEUE_NAME, false, false, false, null)
+    val message = "Book List Request"
+    channel.basicPublish("", LIST_REQ_QUEUE_NAME, null, message.getBytes("UTF-8"))
+    //println(" [x] Sent '" + message + "'")
 
+    channel.queueDeclare(LIST_RES_QUEUE_NAME, false, false, false, null)
+    //println(" [*] Waiting for messages. To exit press CTRL+C")
+    var response = "";
+    val deliverCallback: DeliverCallback = (_, delivery) => {
+      response = new String(delivery.getBody, "UTF-8")
+      //println(" [x] Received '" + response + "'")
+    }
+    channel.basicConsume(LIST_RES_QUEUE_NAME, true, deliverCallback, _ => { })
 
-
-    val result = Http("http://localhost:8000/getAllBooks").asString
+    //val result = Http("http://localhost:8000/getAllBooks").asString
     val jsonPsr: JsonParser =new JsonParser()
-    val jsonAry: JsonArray = jsonPsr.parse(result.body).asInstanceOf[JsonArray]
+    //val jsonAry: JsonArray = jsonPsr.parse(result.body).asInstanceOf[JsonArray]
+    val jsonAry: JsonArray = jsonPsr.parse(response).asInstanceOf[JsonArray]
     val bookList: util.ArrayList[Book] = new util.ArrayList[Book]()
     val gson: Gson = new Gson()
     jsonAry.forEach(jsnObj => bookList.add(gson.fromJson(jsnObj, classOf[Book])))
